276f366aba42ca26da97bd3a12b6a950879d2475
Ravoof147 <125370375+Ravoof147@users.noreply.github.com>
2024-10-20 20:18:38 +0530

    Add files via upload

---
+++ utils/uploader.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+import config
+from pyrogram.types import InputMediaDocument 
+import pickle, os, random, string, asyncio
+from utils.logger import Logger
+from datetime import datetime, timezone
+from utils.clients import get_client
+from pyrogram import Client
+from pyrogram.types import Message
+from urllib.parse import unquote_plus
+
+logger = Logger(__name__)
+
+cache_dir = Path("./cache")
+cache_dir.mkdir(parents=True, exist_ok=True)
+drive_cache_path = cache_dir / "drive.data"
+
+PROGRESS_CACHE = {}
+STOP_TRANSMISSION = []
+
+# Function to generate a random ID
+def getRandomID():
+    global DRIVE_DATA
+    while True:
+        id = "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        if id not in DRIVE_DATA.used_ids:
+            DRIVE_DATA.used_ids.append(id)
+            return id
+
+async def progress_callback(current, total, id, client: Client, file_path):
+    global PROGRESS_CACHE, STOP_TRANSMISSION
+
+    PROGRESS_CACHE[id] = ("running", current, total)
+    if id in STOP_TRANSMISSION:
+        logger.info(f"Stopping transmission {id}")
+        client.stop_transmission()
+        try:
+            os.remove(file_path)
+        except:
+            pass
+
+async def start_file_uploader(file_path, id, directory_path, filename, file_size):
+    global PROGRESS_CACHE
+    from utils.directoryHandler import DRIVE_DATA
+
+    logger.info(f"Uploading file {file_path} {id}")
+
+    if file_size > 1.5 * 1024 * 1024 * 1024:  # Set maximum file size to 1.5 GB
+        # Use premium client for files larger than 1.5 GB
+        client: Client = get_client(premium_required=True)
+    else:
+        client: Client = get_client()
+
+    # Generate a random ID for the file description
+    random_id = getRandomID()
+
+    PROGRESS_CACHE[id] = ("running", 0, 0)
+
+    # Add random ID to the caption
+    message: Message = await client.send_document(
+        config.STORAGE_CHANNEL,
+        file_path,
+        caption=f"File ID: {random_id}",  # Adding the random ID in the caption
+        progress=progress_callback,
+        progress_args=(id, client, file_path),
+        disable_notification=True,
+    )
+    
+    size = (
+        message.photo
+        or message.document
+        or message.video
+        or message.audio
+        or message.sticker
+    ).file_size
+
+    filename = unquote_plus(filename)
+
+    DRIVE_DATA.new_file(directory_path, filename, message.id, size)
+    PROGRESS_CACHE[id] = ("completed", size, size)
+
+    try:
+        os.remove(file_path)
+    except:
+        pass
+    logger.info(f"Uploaded file {file_path} {id}")
+
+# Add any other necessary functions or classes here, if needed.
